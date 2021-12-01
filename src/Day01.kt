cf2b812ee132ac61d881eb37cce38fd561796c67
Sidney Beekhoven <903673+dizney@users.noreply.github.com>
2021-12-01 19:09:36 +0100

    First 2 puzzles

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }.windowed(2).fold(0) { acc, entry ->
+            acc + if (entry.first() < entry[1]) 1 else 0
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.asSequence()
+            .map { it.toInt() }
+            .windowed(3).map { it.sum() }
+            .windowed(2).fold(0) { acc, entry ->
+                acc + if (entry.first() < entry[1]) 1 else 0
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val part1Result = part1(testInput)
+    println(part1Result)
+
+    val part2Result = part2(testInput)
+    println(part2Result)
 }
